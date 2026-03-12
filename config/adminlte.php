b214ac21f51819e304e299123447521c5c931fb8
XxCatrinxX <yepezortizj@gmail.com>
2026-03-12 23:19:59 +0100

    Cambio de la UI y el menu

--- config/adminlte.php
+++ config/adminlte.php
@@ -320,12 +320,21 @@ return [
             'url' => 'admin/blog',
             'can' => 'manage-blog',
         ],
+        ['header' => 'MENÚ PRINCIPAL'],
         [
-            'text' => 'pages',
-            'url' => 'admin/pages',
-            'icon' => 'far fa-fw fa-file',
-            'label' => 4,
-            'label_color' => 'success',
+            'text' => 'Inicio',
+            'url' => 'home',
+            'icon' => 'fas fa-home',
+        ],
+        [
+            'text' => 'Órdenes de Trabajo',
+            'url' => 'ordenes',
+            'icon' => 'fas fa-clipboard-list',
+        ],
+        [
+            'text' => 'Equipos',
+            'url' => 'equipos',
+            'icon' => 'fas fa-desktop',
         ],
         ['header' => 'account_settings'],
         [
@@ -337,44 +346,6 @@ return [
             'text' => 'change_password',
             'url' => 'admin/settings',
             'icon' => 'fas fa-fw fa-lock',
-        ],
-        [
-            'text' => 'multilevel',
-            'icon' => 'fas fa-fw fa-share',
-            'submenu' => [
-                [
-                    'text' => 'level_one',
-                    'url' => '#',
-                ],
-                [
-                    'text' => 'level_one',
-                    'url' => '#',
-                    'submenu' => [
-                        [
-                            'text' => 'level_two',
-                            'url' => '#',
-                        ],
-                        [
-                            'text' => 'level_two',
-                            'url' => '#',
-                            'submenu' => [
-                                [
-                                    'text' => 'level_three',
-                                    'url' => '#',
-                                ],
-                                [
-                                    'text' => 'level_three',
-                                    'url' => '#',
-                                ],
-                            ],
-                        ],
-                    ],
-                ],
-                [
-                    'text' => 'level_one',
-                    'url' => '#',
-                ],
-            ],
         ],
         ['header' => 'labels'],
         [
--- resources/views/vendor/adminlte/master.blade.php
+++ resources/views/vendor/adminlte/master.blade.php
@@ -8,7 +8,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    
     {{-- Custom Meta Tags --}}
     @yield('meta_tags')
 
