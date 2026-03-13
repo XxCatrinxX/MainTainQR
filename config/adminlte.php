6b43b10f463c8c8130477eed40e93686418be539
AlanGSM <2123200537@soy.utj.edu.mx>
2026-03-12 20:48:52 -0600

    Cambio de titulo en sidebar

--- config/adminlte.php
+++ config/adminlte.php
@@ -14,7 +14,7 @@ return [
     |
     */
 
-    'title' => 'MaintainQE',
+    'title' => 'MaintainQR',
     'title_prefix' => '',
     'title_postfix' => '',
 
@@ -63,7 +63,7 @@ return [
     |
     */
 
-    'logo' => '<b>Admin</b>LTE',
+    'logo' => '<b>Maintain</b>QR',
     'logo_img' => 'vendor/adminlte/dist/img/AdminLTELogo.png',
     'logo_img_class' => 'brand-image img-circle elevation-3',
     'logo_img_xl' => null,
