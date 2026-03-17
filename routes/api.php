d6fca9e8a1c21ea1787b17f073442dd335039921
Ulises <2123200450@soy.utj.edu.mx>
2026-03-16 20:35:01 -0600

    agregue algunos cambios

--- routes/api.php
+++ routes/api.php
@@ -10,4 +10,3 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
 });
 // | Aquí es donde puedes registrar las rutas API para tu aplicación. Estas rutas son cargadas por el RouteServiceProvider dentro de un grupo que
-
