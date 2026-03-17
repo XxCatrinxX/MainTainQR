328bfa765a50b203ee7c29e77cc05d08bef15e57
Ulises <2123200450@soy.utj.edu.mx>
2026-03-16 20:27:59 -0600

    aaa

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,3 +10,4 @@ class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
 }
+// | Este es el controlador base de tu aplicación. Todos los demás controladores deberían extender este controlador. En este controlador puedes colocar cualquier lógica común que quieras compartir entre todos tus controladores.
