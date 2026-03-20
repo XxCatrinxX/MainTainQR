3230c79db30d052b618435c199e8d557292080b2
AlanGSM <2123200537@soy.utj.edu.mx>
2026-03-20 17:45:26 -0600

    Desactivar rutas

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@ use App\Http\Controllers\SolicitudCompraController;
 
 Route::redirect('/', '/home');
 
-Auth::routes();
+Auth::routes(['reset' => false]);
 
 // ✅ SOLO UNA RUTA HOME
 Route::get('/home', [HomeController::class , 'index'])
