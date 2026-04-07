a9982cbcbfc1df729119d002fae0dbb101e66754
AlanGSM <2123200537@soy.utj.edu.mx>
2026-04-06 22:57:20 -0600

    Rutas de estilos corregidas

--- config/adminlte.php
+++ config/adminlte.php
@@ -283,8 +283,8 @@ return [
      */
 
     'laravel_asset_bundling' => 'vite',
-    'laravel_css_path' => 'css/app.css',
-    'laravel_js_path' => 'js/app.js',
+    'laravel_css_path' => 'resources/sass/app.scss',
+    'laravel_js_path' => 'resources/js/app.js',
 
     /*
      |--------------------------------------------------------------------------
