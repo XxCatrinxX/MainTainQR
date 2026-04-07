93776504b24bf3b10246ed1a71a0d3bbcbb4fa96
AlanGSM <2123200537@soy.utj.edu.mx>
2026-04-06 22:22:16 -0600

    Correo perron

--- config/mail.php
+++ config/mail.php
@@ -39,7 +39,6 @@ return [
 
         'smtp' => [
             'transport' => 'smtp',
-            'scheme' => env('MAIL_SCHEME'),
             'url' => env('MAIL_URL'),
             'host' => env('MAIL_HOST', '127.0.0.1'),
             'port' => env('MAIL_PORT', 2525),
@@ -52,7 +51,6 @@ return [
 
         'SMTP' => [
             'transport' => 'smtp',
-            'scheme' => env('MAIL_SCHEME'),
             'url' => env('MAIL_URL'),
             'host' => env('MAIL_HOST', '127.0.0.1'),
             'port' => env('MAIL_PORT', 2525),
